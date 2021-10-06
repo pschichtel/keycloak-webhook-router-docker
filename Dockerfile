ff427687eee6a69ad4253790662b85ea01881db6
Phillip Schichtel <phillip.schichtel@cubyte.net>
2021-10-06 02:54:24 +0200

    generate the map

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM nginx:mainline-alpine
 
+RUN apk add --update --no-cache jq
+
 COPY router.conf /etc/nginx/conf.d/default.conf
 COPY targets.conf /etc/nginx/conf.d/targets.conf
---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+target_mappings"$(jq -r -n 'env | to_entries | map(select(.key | startswith("KEYCLOAK_WEBHOOK_URL_"))) | map(.key[21:] + " " + .value + ";") | .[]')"
+targets_file="/etc/nginx/conf.d/targets.conf"
+if [ -n "$target_mappings" -a -w "$targets_file" ]
+then
+    echo "map $http_x_keycloak_realm $webhook_target {" > "$targets_file"
+    echo "$target_mappings" >> "$targets_file"
+    echo "}" >> "$targets_file"
+fi
